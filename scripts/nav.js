bc26d739055b081f81816690fc5ca1e450185d80
Getting Purple <fingerguncowgirl@gmail.com>
2025-01-14 22:31:28 -0500

    script

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,1 +1,9 @@
-console.log($('.navI'));
+$('.navI').click(function() {
+    var mine = this.href;
+    $('.essay').each(function() {
+        console.log('this')
+        if (this.id == mine) {this.show()}
+        else {this.hide()} })
+    console.log(this.href)
+})
+
