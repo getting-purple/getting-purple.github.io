517f249453bd1ac00bdff4715160b1acb6d03a64
Getting Purple <fingerguncowgirl@gmail.com>
2025-01-30 19:18:26 -0500

    twodollars

--- scripts/nav.js
+++ scripts/nav.js
@@ -24,3 +24,20 @@ $('a').attr('target', function() {
   if(this.host == location.host) return '_self'
   else return '_blank'
 })
+
+let b = $('#bouncer')
+async function bounce(top, right) {
+    right = right + 1;
+    top = top + 1;
+    b.css('right',right);
+    b.css('left',top);
+    console.log("top"+top);
+    console.log("right"+right);
+    setTimeout(function() {bounce(top,right)}, 100);
+}
+function start_bouncing() {
+    console.log("comence bouncing");
+    b.show()
+    bounce(0,0);
+}
+setTimeout(start_bouncing, 5000);
