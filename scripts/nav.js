89ab2584fcd337bfc8321ce83a1710577243c39a
Getting Purple <fingerguncowgirl@gmail.com>
2025-01-30 19:34:45 -0500

    bug

--- scripts/nav.js
+++ scripts/nav.js
@@ -26,16 +26,18 @@ $('a').attr('target', function() {
 })
 
 let b = $('#bouncer')
-async function bounce(top, right,dy,dx) {
+function bounce(top, right,dy,dx) {
     right = right + dx;
     top = top + dy;
     b.css('right',right);
     b.css('top',top);
+
     if (b.position().top + b.height() > window.innerHeight) {dy=-1}
     if (b.position().top < 0) {dy=1}
-    if (b.position(). + b.height() > window.innerHeight) {dy=-1}
+
     if (b.position().left < 0) {dx=1}
     if (b.position().left + b.width() > window.innerWidth) {dx=-1}
+
     console.log("  top: "+top);
     console.log("right: "+right);
     setTimeout(function() {bounce(top,right)}, 100);
