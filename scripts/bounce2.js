fcbd9ea1344d643e581bcd9e7a63db49e0d84d1d
Getting Purple <fingerguncowgirl@gmail.com>
2025-02-04 18:57:13 -0500

    80

--- scripts/bounce2.js
+++ scripts/bounce2.js
@@ -164,7 +164,7 @@ function animate() {
 		    //ball
 		    let saved_next_speed=o2.next_speed
 		    o2.next_speed = function(speed,acc){return saved_next_speed(speed,acc)*0.95}
-		    setTimeout(()=>o2.next_speed=saved_next_speed,250)
+		    setTimeout(()=>o2.next_speed=function(speed,acc) {return speed+0.01},80);
 		    o2.angle = (o2.angle + Math.PI+randomTurn(Math.PI/4))%360
 		    
 		}
