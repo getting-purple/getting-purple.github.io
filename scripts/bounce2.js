92a1d780d97e436f1fc6fdf50b30b0f99094b7d2
Getting Purple <fingerguncowgirl@gmail.com>
2025-02-04 19:56:37 -0500

    collision algo change

--- scripts/bounce2.js
+++ scripts/bounce2.js
@@ -183,13 +183,33 @@ function animate() {
 }
 
 // Collisions
+function inside(point, vs) {
+    // ray-casting algorithm based on
+    // https://wrf.ecse.rpi.edu/Research/Short_Notes/pnpoly.html
+    
+    var x = point[0], y = point[1];
+    
+    var inside = false;
+    for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
+        var xi = vs[i][0], yi = vs[i][1];
+        var xj = vs[j][0], yj = vs[j][1];
+        
+        var intersect = ((yi > y) != (yj > y))
+            && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+        if (intersect) inside = !inside;
+    }
+    
+    return inside;
+};
 function checkColiding(ball,region) {
     if (ball.css('display') == 'none' || region.css('display') == 'none') return false;
-    return 	(ball.offset().left + ball.width() > region.offset().left &&
-		 ball.offset().left < region.offset().left + region.width()
-		) && (
-		    ball.offset().top + ball.height() > region.offset().top &&
-			ball.offset().top < region.offset().top + region.height() )
+    
+    var polygon = [ [ region.offset().left, region.offset().top ], 
+		    [ region.offset().left+region.width(),region.offset().top ], 
+		    [ region.offset().left+region.width(),region.offset().top+region.height() ], 
+		    [ region.offset().left,               region.offset().top+region.height() ] ];
+    var test=[ ball.offset().left, ball.offset().top ];
+    return inside(test, polygon);
 }
 
 //
