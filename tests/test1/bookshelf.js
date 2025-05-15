dff714697fe07588745cbbb056ca8ec6adaf2d4a
Getting Purple <fingerguncowgirl@gmail.com>
2025-05-15 19:07:00 -0400

    working

--- tests/test1/bookshelf.js
+++ tests/test1/bookshelf.js
@@ -1,13 +1,30 @@
-let out = false;
+let out = {}
 $('.book').click(function() {
-if (out) {
-     $(this).css('top','0px')
-     $(this).css('z-index','9')
-    out = false;
-} else {
-     $(this).css('top','150px')
-     $(this).css('left','40x')
-     $(this).css('z-index','10')
-    out = true;
-}
-})
+    if (out[this.id]) {
+	$(this).css('top','0px')
+	$(this).css('left','0px')
+	out[this.id] = false;
+    } else {
+	$(this).css('top','140px')
+	$(this).css('left','40px')
+	out[this.id] = true;
+    }
+});
+
+let x=0;
+$('.shelf').children().each(function() {
+    $(this).css('z-index',x-1)
+    $(this).css('background','rgb('+(110+(x+30)^2)+','+(40-x)+','+(217+2*x)+')')
+    $(this).children().each(function() {
+	console.log(this);
+	$(this).css('background','rgb('+(110+(x+30)^2)+','+(40-x)+','+(217+2*x)+')')
+	if (this.className == 'front') {
+	    $(this).css('z-index',x-1)
+	}
+	else {
+	    $(this).css('z-index',x-2)
+	     }
+	
+	});
+    x -= 10;
+});
