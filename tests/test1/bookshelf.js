77ec1aaf72962a25f2487bd6495e715ea8b0267b
Getting Purple <fingerguncowgirl@gmail.com>
2025-05-15 21:11:02 -0400

    colors

--- tests/test1/bookshelf.js
+++ tests/test1/bookshelf.js
@@ -14,10 +14,10 @@ $('.book').click(function() {
 let x=0;
 $('.shelf').children().each(function() {
     $(this).css('z-index',x-1)
-    $(this).css('background','rgb('+(110+(x+30)^2)+','+(40-x)+','+(217+2*x)+')')
+    $(this).css('background','rgb('+(110+(x+30)^2)+','+(40-1*x)+','+(217+2*x)+')')
     $(this).children().each(function() {
 	console.log(this);
-	$(this).css('background','rgb('+(110+(x+30)^2)+','+(40-x)+','+(217+2*x)+')')
+	$(this).css('background','rgb('+(110+(x+30)^2)+','+(40-1.5*x)+','+(217+2*x)+')')
 	if (this.className == 'front') {
 	    $(this).css('z-index',x-1)
 	}
